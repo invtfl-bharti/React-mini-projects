49a2609b4ba74e4e85cbad20d14900b70eb4b7a3
invtfl-bharti <bhartijayprakash19@gmail.com>
2025-02-10 19:14:01 +0530

    Successfully completed the testimonial project

--- testimonals-slider/src/App.js
+++ testimonals-slider/src/App.js
@@ -5,11 +5,11 @@ import Testimonial from "./components/Testimonial";
 const App = () => {
   return (
     <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
-      <div>
+      <div className="text-center">
         <h1 className="font-bold text-4xl">Our Testimonals</h1>
-        <div className="bg-violet-400 h-[10px] w-[1/5] mt-1"></div>
-        <Testimonial reviews={reviews}/>
-       </div>
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+        <Testimonial reviews={reviews} />
+      </div>
     </div>
   );
 };
--- testimonals-slider/src/components/Card.js
+++ testimonals-slider/src/components/Card.js
@@ -1,40 +1,42 @@
-import React from 'react'
+import React from "react";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
 
 const Card = (props) => {
   let review = props.review;
+  
+
   return (
-    <div>
-      <div>
-        <img src={review.image} />
+    <div className="flex flex-col md:relative">
+      <div className="absolute top-[-7rem] z-[10] mx-auto">
+        <img
+          className="aspect-square rounded-full w-[140px] h-[140px] z-25"
+          src={review.image}
+        />
+        <div className="w-[140px] h-[140px] rounded-full bg-violet-500 absolute top-[-6px] z-[-20] left-[10px]"></div>
+      </div>
+
+      <div className="text-center mt-7">
+        <p className="font-bold text-2xl capitalize tracking-wider">{review.name}</p>
       </div>
 
       <div>
-        <p>{review.name}</p>
+        <p className="text-violet-300 uppercase text-sm">{review.job}</p>
       </div>
 
-      <div>
-        <p>{review.job}</p>
+      <div className="text-violet-400 mx-auto mt-5">
+        <FaQuoteLeft />
       </div>
 
-      <div>
-        <FaQuoteLeft/>
+      <div className="mt-5">{review.text}</div>
+
+      <div className="text-violet-400 mx-auto  mt-5">
+        <FaQuoteRight />
       </div>
 
-      <div>{review.text}</div>
-
-      <div><FaQuoteRight/></div>
       
-      <div>
-        <button>+</button>
-        <button>*</button>
-      </div>
-
-      <div>
-        <button>Suprise Me</button>
-      </div>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
